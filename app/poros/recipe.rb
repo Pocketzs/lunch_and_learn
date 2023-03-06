aed005412692ce01384dd5070a94b7ba32ff7b7a
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-03-05 22:48:18 -0600

    add nil id and type to recipe poro

--- app/poros/recipe.rb
+++ app/poros/recipe.rb
@@ -2,11 +2,15 @@ class Recipe
   attr_reader :title,
               :url,
               :country,
-              :image
+              :image,
+              :id,
+              :type
   def initialize(recipe_data, query)
     @title = recipe_data[:label]
     @url = recipe_data[:url]
     @country = query
     @image = recipe_data[:image]
+    @id = nil
+    @type = "recipe"
   end
 end
